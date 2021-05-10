00941754ab2fd18258c997c654aa8d1f25ef42db
Luiz Carlos <66796526+luiz-carlos-1985@users.noreply.github.com>
2021-05-10 12:14:34 -0300

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ require('dotenv').config();
 
 //conectar ao banco de dados
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
+  host: process.env.DB_HOST || "",
+  user: process.env.DB_USER || "",
   password: process.env.DB_PASS || "",
 });
 
