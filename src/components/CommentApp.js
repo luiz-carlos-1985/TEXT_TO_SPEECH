082006f5b0436714e0454b2e75065dddd5ec88c0
Luiz Carlos <66796526+luiz-carlos-1985@users.noreply.github.com>
2021-05-11 13:05:50 -0300

    Update CommentApp.js

--- src/components/CommentApp.js
+++ src/components/CommentApp.js
@@ -23,7 +23,7 @@ function CommentApp() {
         padding: 0,
         margin: 0,
         height: "100vh",
-        backgroundColor: "#fafafa",
+        backgroundColor: "#9aceeb",
       }}
       elevation={0}
     >
