be80b28c8463e0c461855483b3b4db2027406dda
Luiz Carlos <66796526+luiz-carlos-1985@users.noreply.github.com>
2021-07-09 09:10:03 -0300

    Delete setupTests.js

--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// jest-dom adiciona matchers jest customizados para asserção em nós DOM.
-// Permite que você faça coisas como: expect (element) .toHaveTextContent (/ react / i)
-// Saiba mais em: https://github.com/testing-library/jest-dom
-
-import '@testing-library/jest-dom/extend-expect';
